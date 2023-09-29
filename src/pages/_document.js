bc48bc448b6a2189452afd5d5020a8466d8cea45
jguigue <julienguigue97@gmail.com>
2023-09-29 12:41:28 +0200

    Modified SEO

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,15 +8,39 @@ class MyDocument extends Document {
           {/* meta begin */}
           <meta charSet="UTF-8" />
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+          <meta property="og:site_name" content="Portfolio Louis Guigue" />
+          <meta
+            property="og:description"
+            content="Fraîchement diplômé d'un master en communication ,inovation et management de projets je cherche activement un emploi qui saura me satisfaire dans le multimédia. Découvrez mon portfolio pour en savoir plus."
+          />
           {/* meta end */}
 
           {/* public assets begin */}
           <link rel="stylesheet" href="/css/plugins/bootstrap-grid.css" />
           <link rel="stylesheet" href="/css/plugins/font-awesome.min.css" />
-          <link rel="stylesheet" href="/css/plugins/swiper.min.css" />  
+          <link rel="stylesheet" href="/css/plugins/swiper.min.css" />
           {/* public assets end */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WVK3NG8');
+            `,
+            }}
+          />
         </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WVK3NG8"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+            }}
+          />
           <Main />
           <NextScript />
         </body>
